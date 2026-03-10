cf9808a23b682533a6b4d24d038cddac5abe51f2
Theres a username <emprkeathi@gmail.com>
2026-03-10 19:39:27 +0100

    Delete src directory

--- src/utils/clipboard.ts
+++
@@ -1,6 +0,0 @@
-export const copyToClipboard = async (text: string) => { 
-  try { 
-    await navigator.clipboard.writeText(text); 
-    if (navigator.vibrate) navigator.vibrate(50); 
-  } catch (err) {} 
-};
